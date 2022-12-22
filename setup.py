70dd34548969fb6b69eec43792e2b6422cc7d014
ericlib <ericchen.com@outlook.com>
2022-12-22 17:22:14 +0000

    升级

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="download_tools", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="eric",
     author_email="ericlib@aliyun.com",
     description="爬虫的一些工具。",
